6e820a85f118a7e431461e1a7d0dafef91584cab
Cladius <klaudiusz.kowalski@elpassion.pl>
2024-01-19 14:25:20 +0100

    Fix linter & move gitignore

--- code/src/app.ts
+++ code/src/app.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from "express"
-import express from 'express'
+import { Request, Response } from 'express';
+import express from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
