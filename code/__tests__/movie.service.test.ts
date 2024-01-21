b157b3b643175bce9df36af95cfb850e1eba5162
Cladius <klaudiusz.kowalski@elpassion.pl>
2024-01-21 21:07:48 +0100

    Add test create single movie

--- code/__tests__/movie.service.test.ts
+++ code/__tests__/movie.service.test.ts
@@ -1,7 +1,7 @@
 import { MovieService } from '../src/services/movie.service';
 import { MovieRepository } from '../src/repositories/movie.repository';
 
-describe('MovieService', () => {
+describe(MovieService.name, () => {
   let movieService: MovieService;
   let mockMovieRepository: Partial<MovieRepository>;
 
@@ -21,11 +21,12 @@ describe('MovieService', () => {
             'https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2Mjk2NDkyMl5BMl5BanBnXkFtZTgwNTk0NzcyMDE@._V1_SX300.jpg',
         },
       ]),
+      addMovie: jest.fn().mockImplementation(movie => Promise.resolve(movie)),
     };
     movieService = new MovieService(mockMovieRepository as MovieRepository);
   });
 
-  it('should retrieve all movies', async () => {
+  it('should retrieve all movies correctly', async () => {
     const movies = await movieService.getAllMovies();
     expect(movies).toEqual([
       {
@@ -42,5 +43,31 @@ describe('MovieService', () => {
       },
     ]);
     expect(mockMovieRepository.getAllMovies).toHaveBeenCalled();
+  });
+
+  it('should create single movie correctly', async () => {
+    const movie = await movieService.createMovie({
+      title: 'Nebraska',
+      year: 2013,
+      runtime: 117,
+      genres: ['Drama'],
+      director: 'Jane Doe',
+      actors: 'Bruce Dern, Will Forte, June Squibb, Bob Odenkirk',
+      plot: 'An aging, booze-addled father makes the trip from Montana to Nebraska with his estranged son in order to claim a million-dollar Mega Sweepstakes Marketing prize.',
+      posterUrl:
+        'https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2Mjk2NDkyMl5BMl5BanBnXkFtZTgwNTk0NzcyMDE@._V1_SX300.jpg',
+    });
+    expect(movie).toEqual({
+      title: 'Nebraska',
+      year: '2013',
+      runtime: '117',
+      genres: ['Drama'],
+      director: 'Jane Doe',
+      actors: 'Bruce Dern, Will Forte, June Squibb, Bob Odenkirk',
+      plot: 'An aging, booze-addled father makes the trip from Montana to Nebraska with his estranged son in order to claim a million-dollar Mega Sweepstakes Marketing prize.',
+      posterUrl:
+        'https://images-na.ssl-images-amazon.com/images/M/MV5BMTU2Mjk2NDkyMl5BMl5BanBnXkFtZTgwNTk0NzcyMDE@._V1_SX300.jpg',
+    });
+    expect(mockMovieRepository.addMovie).toHaveBeenCalled();
   });
 });
