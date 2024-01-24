6c1466d1c492bc8c6a91c0eba5a74049a2c46aba
Cladius <klaudiusz.kowalski@elpassion.pl>
2024-01-24 21:17:46 +0100

    Change use readFile and writeFile from fs/promises

--- code/src/services/database.service.ts
+++ code/src/services/database.service.ts
@@ -1,6 +1,7 @@
 import config from '@/config';
 import { DBTypes } from '@/types/dbTypes';
-import { readFileSync, writeFileSync } from 'fs';
+// import { readFileSync, writeFileSync } from 'fs';
+import { readFile, writeFile } from 'fs/promises';
 import { join } from 'path';
 import { Service } from 'typedi';
 
@@ -9,11 +10,11 @@ export class DatabaseService {
   private dbPath = join(__dirname, '../../..', 'data', config.dbMoviesFileName);
 
   async readDb(): Promise<DBTypes> {
-    const data = readFileSync(this.dbPath, 'utf-8');
+    const data = await readFile(this.dbPath, 'utf-8');
     return JSON.parse(data);
   }
 
   async writeDb(data: DBTypes) {
-    writeFileSync(this.dbPath, JSON.stringify(data, null, 2), 'utf-8');
+    await writeFile(this.dbPath, JSON.stringify(data, null, 2), 'utf-8');
   }
 }
