ef51f047cd6026ad89027cf739ec0f0ab57ec56f
Cladius <klaudiusz.kowalski@elpassion.pl>
2024-01-26 05:29:55 +0100

    Fix get movies schem

--- code/src/schemas/get-movies.schema.ts
+++ code/src/schemas/get-movies.schema.ts
@@ -1,29 +1,32 @@
 import { z } from 'zod';
 
 export const getMoviesSchema = z.object({
-  duration: z.preprocess(
-    val => {
-      if (typeof val === 'string' && !isNaN(parseFloat(val))) {
-        return parseFloat(val);
-      }
-      return val;
-    },
-    z
-      .number()
-      .positive()
-      .optional()
-      .refine(val => !isNaN(val as any), {
-        message: 'Duration must be a valid number',
-      }),
-  ),
+  duration: z
+    .preprocess(
+      val => {
+        if (typeof val === 'string' && !isNaN(parseFloat(val))) {
+          return parseFloat(val);
+        }
+        return val;
+      },
+      z
+        .number()
+        .positive()
+        .refine(val => !isNaN(val as any), {
+          message: 'Duration must be a valid number',
+        }),
+    )
+    .optional(),
 
-  genres: z.preprocess(
-    val => {
-      if (typeof val === 'string') return [val];
-      return val;
-    },
-    z.array(z.string().min(1)).optional(),
-  ),
+  genres: z
+    .preprocess(
+      val => {
+        if (typeof val === 'string') return [val];
+        return val;
+      },
+      z.array(z.string().min(1)),
+    )
+    .optional(),
 });
 
 export const getMoviesQueryParamsSchema = z.object({
